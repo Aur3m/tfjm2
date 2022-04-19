9e820053419b09051e8c829b826d624958960468
Aurélien M <42558412+Aur3m@users.noreply.github.com>
2022-04-19 19:30:52 +0200

    Update program.py

--- program.py
+++ program.py
@@ -11,27 +11,29 @@ def throw(positions,bird_char,bird_number,n):
         positions[bird_number] = a*positions[bird_number-1] + b*positions[bird_number+1] - un
     return positions
 
-def combinations(a,b,n):
-    temp = []
+def combinations(a,b,n,special_bird):
     bird_char = [(1, 1)] * n
-    for special_bird in range(n):
-        bird_char[special_bird] = (a, b)
-        final = [[1] * n]
-        bird_positions_queue = [[1]*n]
-        while bird_positions_queue:
-            new_bird_positions = heapq.heappop(bird_positions_queue)
-            for bird_number in range(n):
-                potential_new_combination = throw(new_bird_positions,bird_char,bird_number,n)
-                if potential_new_combination not in final:
-                    print(final)
-                    final.append(potential_new_combination.copy())
-                    heapq.heappush(bird_positions_queue,potential_new_combination.copy())
-        temp.append(final)
-    return temp
+    bird_char[special_bird] = (a, b)
+    final = [[1] * n]
+    bird_positions_queue = [[1]*n]
+    while bird_positions_queue:
+        new_bird_positions = heapq.heappop(bird_positions_queue)
+        for bird_number in range(n):
+            potential_new_combination = throw(new_bird_positions,bird_char,bird_number,n)
+            if potential_new_combination not in final:
+                print(final)
+                final.append(potential_new_combination.copy())
+                heapq.heappush(bird_positions_queue,potential_new_combination.copy())
+    return final
 
-print(combinations(2,1,3))
+def special_bird(a,b,n):
+    foo = []
+    for i in range(n):
+        foo.append(combinations(a,b,n,i))
+    return foo
+
+print(special_bird(2,1,3))
 #combinations(2,1,3)
-
 
 #faire en sorte que le special bird reste le même entre deux essais !
 
