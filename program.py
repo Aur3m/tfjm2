b83b563a0d42128235b34770b4e18ab83dba4660
Aurélien M <42558412+Aur3m@users.noreply.github.com>
2022-04-20 01:48:04 +0200

    Update program.py

--- program.py
+++ program.py
@@ -21,7 +21,6 @@ def combinations(a,b,n,special_bird):
         for bird_number in range(n):
             potential_new_combination = throw(new_bird_positions,bird_char,bird_number,n)
             if potential_new_combination not in final:
-                print(final)
                 final.append(potential_new_combination.copy())
                 heapq.heappush(bird_positions_queue,potential_new_combination.copy())
     return final
@@ -29,11 +28,12 @@ def combinations(a,b,n,special_bird):
 def special_bird(a,b,n):
     foo = []
     for i in range(n):
-        foo.append(combinations(a,b,n,i))
+        for element in combinations(a,b,n,i):
+            if element not in foo:
+                foo.append(element)
     return foo
 
-print(special_bird(2,1,3))
-#combinations(2,1,3)
-
-#faire en sorte que le special bird reste le même entre deux essais !
-
+a = int(input("entrez a [mettez 1 par défaut]"))
+b = int(input("entrez b [mettez 1 par défaut]"))
+n = int(input("entrez n"))
+print(special_bird(a,b,n))
