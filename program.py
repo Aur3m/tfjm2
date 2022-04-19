d5d0464617f1777dc596100aabf1bf148b8727de
Aurélien M <42558412+Aur3m@users.noreply.github.com>
2022-04-19 19:00:26 +0200

    Create program.py

---
+++ program.py
@@ -0,0 +1,37 @@
+from heapq import heappush, heappop
+
+def throw(positions,bird_char,bird_number,n):
+    a,b = bird_char[bird_number]
+    un = positions[bird_number]
+    if bird_number == 0:
+        positions[bird_number] = b*positions[bird_number+1] - un
+    elif bird_number == n-1:
+        positions[bird_number] = a*positions[bird_number-1] - un
+    else:
+        positions[bird_number] = a*positions[bird_number-1] + b*positions[bird_number+1] - un
+    return positions
+
+def combinations(a,b,n):
+    temp = []
+    bird_char = [(1, 1)] * n
+    for special_bird in range(n):
+        bird_char[special_bird] = (a, b)
+        final = [[1] * n]
+        bird_positions_queue = [[1]*n]
+        while bird_positions_queue:
+            new_bird_positions = heapq.heappop(bird_positions_queue)
+            for bird_number in range(n):
+                potential_new_combination = throw(new_bird_positions,bird_char,bird_number,n)
+                if potential_new_combination not in final:
+                    print(final)
+                    final.append(potential_new_combination.copy())
+                    heapq.heappush(bird_positions_queue,potential_new_combination.copy())
+        temp.append(final)
+    return temp
+
+print(combinations(2,1,3))
+#combinations(2,1,3)
+
+
+#faire en sorte que le special bird reste le même entre deux essais !
+
