3a06e329d745acd8eff27c5735bc0699442f9fb6
Aurélien M <42558412+Aur3m@users.noreply.github.com>
2022-04-22 21:51:06 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
-import heapq
+import queue
+predecessors = {}
 
 def throw(positions,bird_char,bird_number,n):
     a,b = bird_char[bird_number]
@@ -9,31 +10,35 @@ def throw(positions,bird_char,bird_number,n):
         positions[bird_number] = a*positions[bird_number-1] - un
     else:
         positions[bird_number] = a*positions[bird_number-1] + b*positions[bird_number+1] - un
+    predecessors[''.join(str(positions))] = bird_number+1
     return positions
 
 def combinations(a,b,n,special_bird):
     bird_char = [(1, 1)] * n
     bird_char[special_bird] = (a, b)
     final = [[1] * n]
-    bird_positions_queue = [[1]*n]
-    while bird_positions_queue:
-        new_bird_positions = heapq.heappop(bird_positions_queue)
+    bird_positions_queue = queue.LifoQueue()
+    bird_positions_queue.put([1]*n)
+    while not bird_positions_queue.empty():
+        new_bird_positions = bird_positions_queue.get()
         for bird_number in range(n):
             potential_new_combination = throw(new_bird_positions,bird_char,bird_number,n)
             if potential_new_combination not in final:
                 final.append(potential_new_combination.copy())
-                heapq.heappush(bird_positions_queue,potential_new_combination.copy())
+                bird_positions_queue.put(potential_new_combination.copy())
     return final
 
 def special_bird(a,b,n):
     foo = []
     for i in range(n):
+        foo.append(["L'oiseau spécial de paramètre a,b se trouve en position",i+1])
         for element in combinations(a,b,n,i):
             if element not in foo:
                 foo.append(element)
+                foo.append([predecessors[''.join(str(element))]])
     return foo
 
 a = int(input("entrez a [mettez 1 par défaut]"))
 b = int(input("entrez b [mettez 1 par défaut]"))
-n = int(input("entrez n"))
+n = int(input("entrez n (supérieur ou égale à 2)"))
 print(special_bird(a,b,n))
